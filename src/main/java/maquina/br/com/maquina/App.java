8872e0ac448633a35eddf0dcc3dc31f9c6c8bfee
ElcioP <luan_elcio@hotmail.com>
2019-10-21 17:36:37 -0300

    só pra pegar o arquivo

---
+++ src/main/java/maquina/br/com/maquina/App.java
@@ -0,0 +1,13 @@
+package maquina.br.com.maquina;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/maquina/br/com/maquina/model/maquina.java
@@ -0,0 +1,13 @@
+package maquina.br.com.maquina.model;
+
+public class maquina {
+ private String maquina;
+
+public String getMaquina() {
+	return maquina;
+}
+
+public void setMaquina(String maquina) {
+	this.maquina = maquina;
+}
+}
